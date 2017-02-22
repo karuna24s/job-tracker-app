ed3b9f38b1fe05a1991af6c56f65814943288b66
karuna24s <karuna24s@gmail.com>
2017-02-22 10:00:07 -0500

    generated a migration inorder to add name to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222143446) do
+ActiveRecord::Schema.define(version: 20170222145812) do
 
   create_table "checklists", force: :cascade do |t|
     t.integer  "job_id"
@@ -53,7 +53,9 @@ ActiveRecord::Schema.define(version: 20170222143446) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
