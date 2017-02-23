39b74839c17047d8644fe1d484d69bc31f082b31
karuna24s <karuna24s@gmail.com>
2017-02-22 23:34:48 -0500

    attempting to implement angular_devise, updated app.js and application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,25 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :verify_authenticity_token
+  after_filter :set_csrf_cookie_for_ng
+  respond_to :json
 
   def index
     render 'layouts/application'
   end
-  
+
+  # set token in JS readable session on first HTTP GET request
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
   protected
+
+  #verify token on every subsequent GET request
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
 
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
