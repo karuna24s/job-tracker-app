687756af94051e5a6459fae0d6920f06206f36cd
karuna24s <karuna24s@gmail.com>
2017-02-23 16:39:23 -0500

    commenting out lines 8-11 to see why heroku error

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,8 +5,8 @@
 
   angular
     .module('JobTrackerApp', ['templates', 'ui.router', 'Devise', 'ngMessages'])
-    .config(function($httpProvider) {
-        // for CSRF errors
-        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-    })
+    // .config(function($httpProvider) {
+    //     // for CSRF errors
+    //     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+    // })
 }())
